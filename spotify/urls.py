a3d6ae544f15ac6ca1280d9268a8d2d5c559eb1b
rohsequ <81701847+rohsequ@users.noreply.github.com>
2022-12-08 01:16:45 -0500

    Not used

--- spotify/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AuthURL, spotify_callback, IsAuthenticated
-
-
-urlpatterns = [
-    path('get-auth-url/', AuthURL.as_view()),
-    path('spotify-redirect/', spotify_callback),
-    path('spotify-is-auth/', IsAuthenticated.as_view())
-]
